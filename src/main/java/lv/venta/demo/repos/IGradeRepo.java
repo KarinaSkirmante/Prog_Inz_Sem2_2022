a9fcd2938b508ff264f673685f88323c09a3fa2c
Karina Skirmante <karina.krinkele@venta.lv>
2022-04-29 12:00:44 +0300

    Add: all repos

---
+++ src/main/java/lv/venta/demo/repos/IGradeRepo.java
@@ -0,0 +1,10 @@
+package lv.venta.demo.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.demo.models.Grade;
+
+
+public interface IGradeRepo extends CrudRepository<Grade, Integer>{
+
+}
---
+++ src/main/java/lv/venta/demo/repos/IStudentRepo.java
@@ -0,0 +1,11 @@
+package lv.venta.demo.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.demo.models.Student;
+
+
+
+public interface IStudentRepo extends CrudRepository<Student, Integer>{
+
+}
